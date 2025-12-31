41862eff8385407b677eb5e8ac742ff33f514832
sagrika thakur <sagrikathakur68@gmail.com>
2025-12-31 15:41:13 +0530

    react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  const [theme , setTheme] = useState('light')
+  return (
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  )
+}
+
+export default App
