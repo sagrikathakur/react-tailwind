6c61307064dfbe39424c842b7390a3fefbdd75bc
sagrika thakur <sagrikathakur68@gmail.com>
2026-01-05 19:56:09 +0530

    added two new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero';
+import TrustedBy from './Components/TrustedBy';
 
 const App = () => {
-  const [theme , setTheme] = useState('light')
+  const [theme , setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero/>
+      <TrustedBy/>
     </div>
   )
 }
--- src/Components/ThemeToggleBtn.jsx
+++ src/Components/ThemeToggleBtn.jsx
@@ -1,24 +1,41 @@
 import React, { useEffect } from 'react'
 import assets from '../assets/assets'
 
-const ThemeToggleBtn = ({theme , setTheme}) => {
+const ThemeToggleBtn = ({ theme, setTheme }) => {
+
 useEffect(()=>{
-  if(theme === 'dark'){
-    document.documentElement.classList.add('dark')
-  }else{
-    document.documentElement.classList.remove('dark')
-  }
-})
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+setTheme(theme || (prefersDarkMode ? 'dark' : 'light'));
+}, [])
+
+
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
 
   return (
-    <>
     <button>
-      {theme === ' dark' ? (
-        <img onClick{()=> setTheme('light')} src = {assets.sun_icon}  className='size-8.5 p-1.5 border border-gray-500 rounded-full' alt = '' />): (
-        <img onClick{()=> setTheme('dark')} src = {assets.moon_icon} className='size-8.5 p-1.5 border border-gray-500 rounded-full' alt = '' />
-        )} 
+      {theme === 'dark' ? (
+        <img
+          onClick={() => setTheme('light')}
+          src={assets.sun_icon}
+          className="size-8.5 p-1.5 border border-gray-500 rounded-full"
+          alt="Light mode"
+        />
+      ) : (
+        <img
+          onClick={() => setTheme('dark')}
+          src={assets.moon_icon}
+          className="size-8.5 p-1.5 border border-gray-500 rounded-full"
+          alt="Dark mode"
+        />
+      )}
     </button>
-    </>
   )
 }
 
